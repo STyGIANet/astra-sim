c4cd3ccd3ba04512f40c5344488e6e0c19658a1a
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2025-01-03 17:51:06 +0000

    ubuntu dependencies

---
+++ ubuntu-dependencies.sh
@@ -0,0 +1,12 @@
+sudo apt -y update
+sudo apt -y install coreutils wget vim git
+sudo apt -y install gcc-11 g++-11 make cmake 
+sudo apt -y install clang-format 
+sudo apt -y install libboost-dev libboost-program-options-dev
+sudo apt -y install python3.10 python3-pip
+sudo apt -y install libprotobuf-dev protobuf-compiler
+sudo apt -y install openmpi-bin openmpi-doc libopenmpi-dev
+# Python packages
+pip3 install --upgrade pip
+pip3 install protobuf==5.28.2
+pip3 install graphviz pydot
