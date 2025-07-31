5b611097a2751b1aade3027812ac05405073deac
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2025-07-31 15:09:54 +0000

    scripts

--- acad/scripts/run-reps-all-failures.sh
+++ acad/scripts/run-reps-all-failures.sh
@@ -8,7 +8,7 @@ source config.sh
 NODES=(256)
 MSG_SIZES=(1000000 2000000 4000000 8000000 16000000 32000000 64000000 128000000 256000000)
 TXT_WORKLOADS=("Gpt_117M")
-ALLREDUCE_ALGS=("direct" "halvingDoubling" "ring" "doubleBinaryTree")
+ALLREDUCE_ALGS=("direct" "halvingDoubling")
 APP_LOADBALANCE_ALGS=("ethereal" "mp-rdma-2" "mp-rdma-4" "mp-rdma-8" "none")
 ROUTING_ALGS=("SOURCE_ROUTING" "REPS" "REPSv4" "END_HOST_SPRAY" "ECMP")
 
